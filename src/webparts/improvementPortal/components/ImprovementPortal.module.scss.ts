0c9e752d49ddec2b70da72710e9799143c369568
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2025-09-25 00:31:54 +0200

    Support Node 20 toolchain

--- src/webparts/improvementPortal/components/ImprovementPortal.module.scss.ts
+++ src/webparts/improvementPortal/components/ImprovementPortal.module.scss.ts
@@ -1,24 +1,25 @@
-require('./ImprovementPortal.module.css');
-
+/* tslint:disable */
+require("./ImprovementPortal.module.css");
 const styles = {
-  portal: 'portal',
-  header: 'header',
-  controls: 'controls',
-  voteSummary: 'voteSummary',
-  form: 'form',
-  formButtons: 'formButtons',
-  searchRow: 'searchRow',
-  sectionTitle: 'sectionTitle',
-  suggestionCard: 'suggestionCard',
-  cardHeader: 'cardHeader',
-  statusBadge: 'statusBadge',
-  meta: 'meta',
-  description: 'description',
-  voteActions: 'voteActions',
-  voteCounter: 'voteCounter',
-  statusControls: 'statusControls',
-  emptyState: 'emptyState',
-  messageBar: 'messageBar'
+  portal: 'portal_ad3bd85f',
+  header: 'header_ad3bd85f',
+  controls: 'controls_ad3bd85f',
+  voteSummary: 'voteSummary_ad3bd85f',
+  form: 'form_ad3bd85f',
+  formButtons: 'formButtons_ad3bd85f',
+  searchRow: 'searchRow_ad3bd85f',
+  sectionTitle: 'sectionTitle_ad3bd85f',
+  suggestionCard: 'suggestionCard_ad3bd85f',
+  cardHeader: 'cardHeader_ad3bd85f',
+  statusBadge: 'statusBadge_ad3bd85f',
+  meta: 'meta_ad3bd85f',
+  description: 'description_ad3bd85f',
+  voteActions: 'voteActions_ad3bd85f',
+  voteCounter: 'voteCounter_ad3bd85f',
+  statusControls: 'statusControls_ad3bd85f',
+  emptyState: 'emptyState_ad3bd85f',
+  messageBar: 'messageBar_ad3bd85f'
 };
 
 export default styles;
+/* tslint:enable */
